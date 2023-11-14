88d011c0404c4f112a65d5f0c54fab12e9023df4
Zeeshan Hafiz <zhafiz0705@gmail.com>
2023-11-14 11:38:30 -0500

    bookCover

---
+++ BookCoverpage.swift
@@ -0,0 +1,18 @@
+//
+//  BookCoverpage.swift
+//  SwiftUIStoryTemplate
+//
+//  Created by Zeeshan Hafiz on 11/14/23.
+//
+
+import SwiftUI
+
+struct BookCoverpage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookCoverpage()
+}
