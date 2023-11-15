8b6338751ec77d220826c9dbcea5f8e851092ac7
shrayven <shrayvenwells@icloud.com>
2023-11-15 10:50:50 -0500

    here is Chapter 3

---
+++ SwiftUIStoryTemplate/Chapter3/Navigationpage.swift
@@ -0,0 +1,18 @@
+//
+//  Navigationpage.swift
+//  SwiftUIStoryTemplate
+//
+//  Created by Sh'Rayven Wells on 11/15/23.
+//
+
+import SwiftUI
+
+struct Navigationpage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Navigationpage()
+}
---
+++ SwiftUIStoryTemplate/Chapter3/Tabsview.swift
@@ -0,0 +1,18 @@
+//
+//  Tabsview.swift
+//  SwiftUIStoryTemplate
+//
+//  Created by Sh'Rayven Wells on 11/15/23.
+//
+
+import SwiftUI
+
+struct Tabsview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Tabsview()
+}
