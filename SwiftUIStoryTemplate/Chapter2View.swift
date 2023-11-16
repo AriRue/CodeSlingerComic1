394f805792425f23708f8b440bf7a035572e4b3c
Zeeshan Hafiz <zhafiz0705@gmail.com>
2023-11-16 08:36:37 -0500

    updataed chapter 1

--- SwiftUIStoryTemplate/Chapter2View.swift
+++ SwiftUIStoryTemplate/Chapter2View.swift
@@ -2,9 +2,65 @@ import SwiftUI
 
 struct Chapter2View: View {
     var body: some View {
-        Text("Chapter 2")
-    }
-}
+        
+            
+                NavigationStack{
+                
+                ZStack {
+                    
+                    
+                    // .foregroundColor(.white)
+                    Color .black
+                        .ignoresSafeArea()
+                    
+                    
+                    VStack{
+                        
+                        
+                        
+                        Text("She jumps back from her computer chair and as she does her eyes can see the code of everything electronic in her room. She looks at her phone and can see the code of every app in her phone simultaneously. She even sees a few bugs and is able to fix them with just a thought.")
+                            .foregroundStyle(.cyan)
+                        
+                        
+////                        Image(.greensuit)
+//                            .resizable()
+//                            .scaledToFit()
+                        
+                        
+                        
+                        Text("Lily realizes her mind is thinking in code and she can control every bit of code she sees with just her thoughts. Suddenly, A distorted message flickers on her screen repeatedly, remnants of Seraphis warning. Seraphis types out a message.")
+                        
+                            .foregroundStyle(.cyan)
+//                        
+//                        Image(.computerscreen)
+//                            .resizable()
+//                            .scaledToFit()
+                        
+//                        
+//                        NavigationLink(destination: Chapter2Page2View()){
+//                            
+//                            HStack{
+//                               // Text("Go")
+//                                
+//                                
+//                                Image(systemName: "arrow.right.circle")
+//                                
+//                            }
+//                            
+//                        }
+                        
+                    }
+                    
+                    
+                }
+                
+            }
+              
+                
+            }
+        }
+    
+
 
 #Preview {
     Chapter2View()
